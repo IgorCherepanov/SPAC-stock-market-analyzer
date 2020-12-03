99f32885fa2d0b7c91a48d37b84e29aa986edad2
IgorCherepanov <65504751+IgorCherepanov@users.noreply.github.com>
2020-12-03 21:56:04 +0100

    Add files via upload

--- toolkit.py
+++ toolkit.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 import matplotlib as plt
 import matplotlib.dates as mdates
 
-def connect():
+def connect(key):
     '''
     Connect to the Yahoo Finance server. We use here API from Rakuten:
     https://english.api.rakuten.net/apidojo/api/yahoo-finance1
@@ -15,7 +15,7 @@ def connect():
 
     headers = {
     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
-    'x-rapidapi-key': "bea278beefmsh674fb2d3881fe7ep1dd4f6jsndff4801d1186"
+    'x-rapidapi-key': key
     }
 
     conn.request("GET", "/auto-complete?region=US&q=tesla", headers=headers)
@@ -25,13 +25,8 @@ def connect():
     return
 
 
-def get_chart(ticker, interval, timerange, finalData):
-    
-    if ticker in finalData["Ticker"].values:
-        print("The ticker has been already created")
-        return(finalData)
-    
-    else:
+def data_import(ticker, interval, timerange, key):
+# Import data from Yahoo Finance
     
         url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart" # Making request to the Rakouten API 
 
@@ -39,7 +34,7 @@ def get_chart(ticker, interval, timerange, finalData):
 
         headers = {
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
-        'x-rapidapi-key': "bea278beefmsh674fb2d3881fe7ep1dd4f6jsndff4801d1186"
+        'x-rapidapi-key': key
         }
 
         response = requests.request("GET", url, headers=headers, params=querystring) # Getting response
@@ -55,42 +50,67 @@ def get_chart(ticker, interval, timerange, finalData):
             "Price": prices
             })
             
-        if  localData["Price"].dropna()[0] > 12: # If the chart starts with the price higher than the entry threshold -> skip this company
-            print("The price starts higher than the entry threshold")
-            return(finalData)
+        return(localData)
+
+def get_chart(ticker, interval, timerange, finalData, key, update_prices = False):
+# Get entries to the datebase, if update_prices = False then just recalculating revenues and stuff using the existing base
+    
+    if update_prices == False: # Do requests and update prices or use th eexisting db?
+        index = finalData["Ticker"][finalData["Ticker"] == ticker].index[0] # Finding the number of the requested ticker
+        localData = finalData["Price Data"].iloc[index]
         
-        else:            
-            
-            try:
-                start_date = localData[localData["Price"] > 12]["Data"].iloc[0] # Figuring out when the price movement began. For example, the day when the price got 11 bucks
-                start_date_index = max(0, get_jump_date(localData))
-                date_object = datetime.strptime(start_date, "%Y-%m-%d %I:%M:%S")
+    elif update_prices == True and ticker in finalData["Ticker"].values:
+        print("The ticker has been already created")
+        return(finalData)
+    
+    elif update_prices == True and ticker not in finalData["Ticker"].values:
+         localData = data_import(ticker, interval, timerange, key)
+
+        
+    if check_if_closed(localData) == True: # if the company is closed, put it to the list and proceed
+        f = open("closed.txt", "a")
+        f.write(ticker)
+        f.close()
+        print("% has been closed" %ticker)
+                        
+    if localData["Price"].dropna()[0] > 12: # If the chart starts with the price higher than the entry threshold -> skip this company
+        print("The price starts higher than the entry threshold")
+        return(finalData)
+        
+    else:            
+    
+        try:
+            start_date = localData[localData["Price"] > 12]["Data"].iloc[0] # Figuring out when the price movement began. For example, the day when the price got 11 bucks
+            start_date_index = max(0, get_jump_date(localData))
+            date_object = datetime.strptime(start_date, "%Y-%m-%d %I:%M:%S")
    
-                pd.options.display.float_format = '{:,.2f}'.format
-                finalData = finalData.append({"Ticker": ticker,
+            pd.options.display.float_format = '{:,.2f}'.format
+            finalData = finalData.append({"Ticker": ticker,
                              "Price Data": localData,
+                             "IPO Date": get_ipo_date(localData).strftime('%B %d, %Y'),
                              "Price Jump Date": date_object.strftime('%B %d, %Y'),
                              "Days Till Jump": how_long(localData),
-                             "Buy Price": "$%.2f" % get_buy_price(localData, start_date_index),
+                             "Buy Price": "$%.2f" % get_av_buy_price(localData),
                              "1d Av. Rets, %": get_av_returns(localData, start_date_index, 1),
                              "3d Av. Rets, %": get_av_returns(localData, start_date_index, 3),
                              "1w Av. Rets, %": get_av_returns(localData, start_date_index, 7),
                              "1M Av. Rets, %": get_av_returns(localData, start_date_index, 30),
-                             "Current Rets, %": get_current_returns(localData, start_date_index)
+                             "Current Rets, %": get_current_returns(localData, start_date_index),
                             }, ignore_index = True)
-            except Exception:
-                finalData = finalData.append({"Ticker": ticker,
+        except Exception:
+            finalData = finalData.append({"Ticker": ticker,
                              "Price Data": localData,
+                             "IPO Date": get_ipo_date(localData).strftime('%B %d, %Y'),
                              "Price Jump Date": "Not Found",
                              "Days Till Jump": how_long(localData),
                              "1d Av. Rets, %": None,
                              "3d Av. Rets, %": None,                 
                              "1w Av. Rets, %": None,
                              "1M Av. Rets, %": None,
-                             "Current Rets, %": None                 
+                             "Current Rets, %": None,
                             }, ignore_index = True)
         
-        return(finalData)
+    return(finalData)
     
 def get_ipo_date(localData):
 
@@ -169,7 +189,7 @@ def plot_chart(ticker, finalData):
     
     index = finalData["Ticker"][finalData["Ticker"] == ticker].index[0] # Finding the number of the requested ticker
     ax = finalData["Price Data"].iloc[index].plot(x = "Data", y = "Price", figsize = (12,9))
-    buy_price = get_buy_price(finalData["Price Data"].iloc[index], get_jump_date(finalData["Price Data"].iloc[index]))
+    buy_price = get_av_buy_price(finalData["Price Data"].iloc[index])
     start_date_index = get_jump_date(finalData["Price Data"].iloc[index])                                     
     week_date_index = get_date_index(finalData["Price Data"].iloc[index], get_jump_date(finalData["Price Data"].iloc[index]), plus_days = 7)
     month_date_index = get_date_index(finalData["Price Data"].iloc[index], get_jump_date(finalData["Price Data"].iloc[index]), plus_days = 30)
@@ -180,7 +200,7 @@ def plot_chart(ticker, finalData):
     else:
         ax.axhline(buy_price, color = "red", ls="--")
                                             
-    if buy_price == None:
+    if start_date_index == None:
         pass
     else:
         ax.axvline(start_date_index, color = "black", ls="--")    
@@ -215,6 +235,14 @@ def get_returns(localData, start_date_index, period):
             
     return(rets)
 
+def get_av_buy_price(localData):
+# Average price before jump
+    jump_date = get_jump_date(localData, dateobject_format = None)
+    av_buy_price = localData["Price"][:jump_date].dropna().mean()
+    if get_jump_date(localData, dateobject_format = None) == 0:
+        av_buy_price = localData["Price"][jump_date]
+    return(av_buy_price)
+
 def get_av_returns(localData, start_date_index, period):
     
     '''
@@ -223,6 +251,7 @@ def get_av_returns(localData, start_date_index, period):
     
     start_date_index = get_jump_date(localData)
     target_date_index = get_date_index(localData, start_date_index, plus_days = period)
+    av_buy_price = get_av_buy_price(localData)
     cum_rets = 0
     nan_number = 0
     if target_date_index == None:
@@ -236,7 +265,7 @@ def get_av_returns(localData, start_date_index, period):
                 nan_number += 1
                 continue
             else:
-                cum_rets += 100*(localData["Price"][start_date_index+n]-localData["Price"][start_date_index])/localData["Price"][start_date_index]
+                cum_rets += 100*(localData["Price"][start_date_index+n]-av_buy_price)/av_buy_price
         
         av_rets = cum_rets/(target_date_index-start_date_index-nan_number)
         return(av_rets)
@@ -244,19 +273,14 @@ def get_av_returns(localData, start_date_index, period):
     
     
 def get_current_returns(localData, start_date_index):
-    # If the price just before the price jump is unknown than we take the average price obsrved before thsi timepoint
-    if localData["Price"].iloc[start_date_index] > 0:
-    
-        rets = 100 * (localData["Price"].dropna().iloc[-1]-localData["Price"].iloc[start_date_index])/localData["Price"].iloc[start_date_index]
-    else:
-        
-        average = localData["Price"].iloc[:start_date_index].mean()
-        rets = 100 * (localData["Price"].dropna().iloc[-1]-average)/average
+    # If the price just before the price jump is unknown than we take the average price observed before this timepoint
+    av_buy_price = get_av_buy_price(localData)   
+    rets = 100 * (localData["Price"].dropna().iloc[-1]-av_buy_price)/av_buy_price
          
     return(rets)
 
-def get_buy_price(localData, start_date_index):
-    # If the price just before the price jump is unknown than we take the average price obsrved before this timepoint
+def get_jump_price(localData, start_date_index):
+    # If the price just before the price jump is unknown than we take the average price observed before this timepoint
     if start_date_index == None:
         return(None)
     else:
